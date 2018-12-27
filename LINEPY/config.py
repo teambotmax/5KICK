a6519df0f85cbf2c8dbb7210b3654c4f92c7455a
S҉E҉L҉F҉B҉O҉T҉-҉B҉Y҉:҉M҉A҉X҉ <39564544+0954517662@users.noreply.github.com>
2018-12-27 20:18:23 +0700

    Add file via upload

--- LINEPY/config.py
+++ LINEPY/config.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from akad.ttypes import ApplicationType
+from ..akad.ttypes import ApplicationType
 import re
 
 class Config(object):
-    LINE_HOST_DOMAIN            = 'https://gd2.line.naver.jp'
+    LINE_HOST_DOMAIN            = 'https://gwx.line.naver.jp'
     LINE_OBS_DOMAIN             = 'https://obs-sg.line-apps.com'
-    LINE_TIMELINE_API           = 'https://gd2.line.naver.jp/mh/api'
-    LINE_TIMELINE_MH            = 'https://gd2.line.naver.jp/mh'
+    LINE_TIMELINE_API           = 'https://gwx.line.naver.jp/mh/api'
+    LINE_TIMELINE_MH            = 'https://gwx.line.naver.jp/mh'
 
     LINE_LOGIN_QUERY_PATH       = '/api/v4p/rs'
     LINE_AUTH_QUERY_PATH        = '/api/v4/TalkService.do'
@@ -17,6 +17,7 @@ class Config(object):
     LINE_CERTIFICATE_PATH       = '/Q'
     LINE_CHAN_QUERY_PATH        = '/CH4'
     LINE_SQUARE_QUERY_PATH      = '/SQS1'
+    LINE_SHOP_QUERY_PATH        = '/SHOP4'
 
     CHANNEL_ID = {
         'LINE_TIMELINE': '1341209850',
@@ -24,17 +25,17 @@ class Config(object):
         'LINE_TODAY': '1518712866',
         'LINE_STORE': '1376922440',
         'LINE_MUSIC': '1381425814',
-        'LINE_BUSINESS': '1459630796'
+        'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '2.1.5'
+    APP_TYPE    = "CHROMEOS\t2.1.0\tCHROMEOS\t10.0.0"
+    APP_VER     = '8.9.1'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'SELFBOT-BY:MAX'
-    SYSTEM_VER  = '11.2.5'
+    SYSTEM_VER  = '10.12.0'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
     def __init__(self):
-        self.APP_NAME = '%s\t%s\t%s\t%s' % (self.APP_TYPE, self.APP_VER, self.SYSTEM_NAME, self.SYSTEM_VER)
+        self.APP_NAME = 'CHROMEOS\t2.1.0\tCHROMEOS\t10.0.0'
         self.USER_AGENT = 'Line/%s' % self.APP_VER
